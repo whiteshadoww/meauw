1fe3584eb8d0c79d40af63a5cfc68fc7b1216f2b
Edwin Bello <ayiembaeb@gmail.com>
2019-07-07 21:41:41 +0300

    update: final apk

--- app/src/main/java/me/snowshadow/meauw/activities/SplashActivity.kt
+++ app/src/main/java/me/snowshadow/meauw/activities/SplashActivity.kt
@@ -19,6 +19,7 @@ class SplashActivity : AppCompatActivity() {
             .delay(3, TimeUnit.SECONDS)
             .subscribe {
                 startActivity(Intent(this, MainActivity::class.java))
+                supportFinishAfterTransition()
             }
     }
 }
